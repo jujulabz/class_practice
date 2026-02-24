3710d0c3a2722720d9b140eea16506cab7d7d5ef
jujulabz <mp678503@gmail.com>
2026-02-24 10:40:19 -0500

    Removed flags and used break statements

--- parrot.py
+++ parrot.py
@@ -1,16 +1,14 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
-active = True
-
-while active:
+while True:
     message = input(prompt)
     
     if message == 'quit':
-        active= False
+        break
     elif message == 'parrot':
-        active = False
         print("Oh, think you're a wise guy hun?")
+        break
     else:
         print(message)
         
